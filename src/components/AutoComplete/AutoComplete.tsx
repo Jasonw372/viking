52a49ac37a8c17959f0a1573bac38bef66a42079
huanyushi <1174601344@qq.com>
2025-01-13 20:38:17 +0800

    feat:完成了鼠标和键盘控制AutoComplete选择的功能

--- src/components/AutoComplete/AutoComplete.tsx
+++ src/components/AutoComplete/AutoComplete.tsx
@@ -1,9 +1,9 @@
 import Input from '../Input';
 import type { InputProps } from '../Input/input';
-import type { ChangeEvent } from 'react';
+import type { ChangeEvent, KeyboardEvent } from 'react';
 import React, { useState, useEffect, forwardRef } from 'react';
 import Transition from '../Transition';
-
+import classNames from 'classnames';
 interface AutoCompleteProps extends Omit<InputProps, 'onSelect' | 'defaultValue'> {
   fetchSuggestions: (keyWord: string) => string[];
   onSelect?: (item: string) => void;
@@ -15,6 +15,7 @@ const AutoComplete = forwardRef<HTMLInputElement, AutoCompleteProps>((props, ref
   const [innerValue, setInnerValue] = useState(defaultValue || '');
   const [suggestions, setSuggestions] = useState<string[]>([]);
   const [showDropdown, setShowDropdown] = useState(false);
+  const [highlightIndex, setHighlightIndex] = useState(-1);
 
   // 处理受控模式下的 value 更新
   useEffect(() => {
@@ -57,12 +58,45 @@ const AutoComplete = forwardRef<HTMLInputElement, AutoCompleteProps>((props, ref
     }
   };
 
+  const highlight = (index: number) => {
+    if (index < 0) index = 0;
+    if (index >= suggestions.length) {
+      index = suggestions.length - 1;
+    }
+    setHighlightIndex(index);
+  };
+
+  const handleMouseMove = (index: number) => {
+    setHighlightIndex(index);
+  };
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    switch (e.key) {
+      case 'Enter':
+        if (suggestions[highlightIndex]) {
+          handleSelect(suggestions[highlightIndex]);
+        }
+        break;
+      case 'ArrowUp':
+        highlight(highlightIndex - 1);
+        break;
+      case 'ArrowDown':
+        highlight(highlightIndex + 1);
+        break;
+      case 'Escape':
+        setShowDropdown(false);
+        break;
+      default:
+        break;
+    }
+  };
   return (
     <div className="auto-complete">
       <Input
         ref={ref}
         value={value !== undefined ? value : innerValue}
         onChange={handleChange}
+        onKeyDown={handleKeyDown}
         {...restProps}
       />
       <Transition
@@ -72,11 +106,21 @@ const AutoComplete = forwardRef<HTMLInputElement, AutoCompleteProps>((props, ref
         wrapper
       >
         <ul className="suggestion-list">
-          {suggestions.map((item, index) => (
-            <li key={index} onClick={() => handleSelect(item)}>
-              {item}
-            </li>
-          ))}
+          {suggestions.map((item, index) => {
+            const classes = classNames('suggestion-item', {
+              'is-active': index === highlightIndex,
+            });
+            return (
+              <li
+                key={index}
+                className={classes}
+                onClick={() => handleSelect(item)}
+                onMouseMove={() => handleMouseMove(index)}
+              >
+                {item}
+              </li>
+            );
+          })}
         </ul>
       </Transition>
     </div>
