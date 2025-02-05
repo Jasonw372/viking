a664df976309cc6712f14a822fa507933bb14d91
huanyushi <1174601344@qq.com>
2025-02-05 19:14:17 +0800

    feat: 添加代码覆盖率支持，更新测试配置和依赖项

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,14 @@
-import {defineConfig} from 'vitest/config';
+import {coverageConfigDefaults, defineConfig} from 'vitest/config';
 
 export default defineConfig({
   test: {
     environment: 'jsdom', // Use jsdom as the testing environment
     globals: true, // Allow using global variables like "describe" and "it"
     setupFiles: './vitest.setup.ts', // Optional: path to setup file
-  },
+    coverage: {
+      enabled: true,
+      reporter: ['text', 'json', 'html'],
+      exclude: [ '**/*.stories.tsx',...coverageConfigDefaults.exclude],
+    }
+  }
 });
