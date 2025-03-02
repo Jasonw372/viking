3da207a91fdda79f9d2b91eaa040e87db3291456
Jasonw372 <Jasonw372@users.noreply.github.com>
2025-03-02 05:21:44 +0000

    deploy: d8cc17d6f1893c0350c0754cc7fc9ad8d20deb99

--- assets/index-BT2t1l9q.js
+++
@@ -1,1 +0,0 @@
-import{B as t}from"./button-C-Ryr-Tt.js";const n=t;export{n as T};
--- assets/progress-DbRsBJe7.js
+++
@@ -1,8 +0,0 @@
-import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";const l=s=>{const{percent:r,strokeHeight:a=15,showText:t=!0,styles:n,theme:i="primary"}=s;return e.jsx("div",{className:"progress-bar",style:n,"data-testid":"test-progress",children:e.jsx("div",{className:"progress-bar-outer",style:{height:`${a}px`},children:e.jsx("div",{className:`progress-bar-inner color-${i}`,style:{width:`${r}%`},children:t&&e.jsx("span",{className:"inner-text",children:`${r}%`})})})})};l.__docgenInfo={description:"",methods:[],displayName:"Progress",props:{percent:{required:!0,tsType:{name:"number"},description:""},strokeHeight:{required:!1,tsType:{name:"number"},description:""},showText:{required:!1,tsType:{name:"boolean"},description:""},styles:{required:!1,tsType:{name:"ReactCSSProperties",raw:"React.CSSProperties"},description:""},theme:{required:!1,tsType:{name:"union",raw:`| 'primary'
-| 'secondary'
-| 'success'
-| 'info'
-| 'warning'
-| 'danger'
-| 'light'
-| 'dark'`,elements:[{name:"literal",value:"'primary'"},{name:"literal",value:"'secondary'"},{name:"literal",value:"'success'"},{name:"literal",value:"'info'"},{name:"literal",value:"'warning'"},{name:"literal",value:"'danger'"},{name:"literal",value:"'light'"},{name:"literal",value:"'dark'"}]},description:""}}};export{l as P};
