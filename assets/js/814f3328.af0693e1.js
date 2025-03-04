fe6f3b0e0c940f0fe0d69dc7e9a4ee1488ecc23d
Jasonw372 <Jasonw372@users.noreply.github.com>
2025-03-04 02:46:26 +0000

    deploy: 31eaaaa27f184248b70ef613950e53dbf6759a7c

---
+++ assets/js/814f3328.af0693e1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[7472],{5513:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome to Viking Design\'s blog!","permalink":"/viking/blog/welcome","unlisted":false,"date":"2025-03-04T00:00:00.000Z"}]}')}}]);
